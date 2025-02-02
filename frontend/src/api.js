c86a60e20abee606bc248fbcacbf694aa798f30c
AfreenAhmed405 <mail.afreenahmed@gmail.com>
2025-02-01 22:54:45 -0600

    Added urls

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8080/tasks";
+const API_BASE_URL = "http://trelloforsam-backend.us-east-2.elasticbeanstalk.com/tasks";
 
 export const addTask = (task) => {
     return axios.post(API_BASE_URL, task);
